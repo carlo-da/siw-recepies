b2c08cb2628a625776bd4ca4f54a37ff8e6da521
carlo-da <flyitnavy@gmail.com>
2025-12-29 11:08:06 +0100

    Aggiornato classe controller recipe

--- siw-recipes/src/main/java/it/uniroma3/siw/siw_recipes/controller/RecipeController.java
+++ siw-recipes/src/main/java/it/uniroma3/siw/siw_recipes/controller/RecipeController.java
@@ -1,20 +1,26 @@
 package it.uniroma3.siw.siw_recipes.controller;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import it.uniroma3.siw.siw_recipes.model.Recipe;
 import it.uniroma3.siw.siw_recipes.model.User;
 import it.uniroma3.siw.siw_recipes.repository.UserRepository;
 import it.uniroma3.siw.siw_recipes.service.RecipeService;
+import it.uniroma3.siw.siw_recipes.util.FileUploadUtil;
 import jakarta.validation.Valid;
 
 @Controller
@@ -48,15 +54,21 @@ public class RecipeController {
 
     @PostMapping("/recipe")
     public String newRecipe(@Valid @ModelAttribute("recipe") Recipe recipe, 
-                            BindingResult bindingResult, Model model) {
+                            BindingResult bindingResult,@RequestParam("image") MultipartFile multipartFile,
+                             Model model) throws IOException {
         
         if (bindingResult.hasErrors()) {
             return "recipeForm";
         }
-        UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();// Recuperiamo l'utente corrente dalla sessione di sicurezza
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        recipe.setImageFileName(fileName); // Gestione immagine
+        UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User currentUser = userRepository.findByEmail(userDetails.getUsername()).get();
-        recipe.setAuthor(currentUser); //Impostiamo l'autore della ricetta
-        recipeService.saveRecipe(recipe);
-        return "redirect:/recipe/" + recipe.getId();// Redirect alla pagina della nuova ricetta
+        recipe.setAuthor(currentUser); // Gestione autore
+        Recipe savedRecipe = recipeService.saveRecipe(recipe);
+        String uploadDir = "recipe-images/" + savedRecipe.getId();
+        FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+        
+        return "redirect:/recipe/" + savedRecipe.getId();
     }
 }
